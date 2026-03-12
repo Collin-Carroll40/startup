09d965385e0aeba435649c694524d3c5d7848613
Collin-Carroll40 <colindrewcarroll@gmail.com>
2026-03-12 16:43:41 -0600

    added logout and middleware

--- service/index.js
+++ service/index.js
@@ -1,10 +1,45 @@
 const express = require('express');
-const app = express();
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const uuid = require('uuid');
 
+const app = express();
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
+const authCookieName = 'token';
+
+// In-memory storage
+let users = [];
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.static('public'));
+
+// Router
+const apiRouter = express.Router();
+app.use('/api', apiRouter);
+
+// Register
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await findUser('email', req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await createUser(req.body.email, req.body.password);
+    setAuthCookie(res, user.token);
+    res.send({ email: user.email });
+  }
+});
+
+// Login
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await findUser('email', req.body.email);
+  if (user && await bcrypt.compare(req.body.password, user.password)) {
+    user.token = uuid.v4();
+    setAuthCookie(res, user.token);
+    res.send({ email: user.email });
+    return;
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
 
 // Test route
 app.get('/api/test', (_req, res) => {
@@ -15,6 +50,27 @@ app.get('/api/test', (_req, res) => {
 app.use((_req, res) => {
   res.sendFile('index.html', { root: 'public' });
 });
+
+// Helpers
+async function createUser(email, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+  const user = { email, password: passwordHash, token: uuid.v4() };
+  users.push(user);
+  return user;
+}
+
+async function findUser(field, value) {
+  if (!value) return null;
+  return users.find((u) => u[field] === value);
+}
+
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
 
 app.listen(port, () => {
   console.log(`Pipeline Pro service listening on port ${port}`);
